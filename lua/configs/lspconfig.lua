02439fc90fc3bae3773aa0c5bed870cb138dcc91
dasakash26 <akashd2664@gmail.com>
2025-06-23 13:16:26 +0530

    minor change

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,47 @@
-require("nvchad.configs.lspconfig").defaults()
+local default_config = require("nvchad.configs.lspconfig")
+local on_attach = default_config.on_attach
+local capabilities = default_config.capabilities
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local lspconfig = require("lspconfig")
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- LSP servers for MERN + TypeScript + C++
+local servers = {
+  "ts_ls",  -- JavaScript/TypeScript
+  "html",   -- HTML
+  "cssls",  -- CSS
+  "jsonls", -- JSON
+  "eslint", -- Linting JS/TS
+  "clangd", -- C/C++
+  "prismals",
+}
+
+local on_attach = function(client, bufnr)
+  -- Format on save
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
+-- Optional: customize Lua LS for Neovim dev
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+      workspace = { checkThirdParty = false },
+    },
+  },
+})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,28 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- Normal Mode mappings
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", "<leader>w", ":w<CR>", { desc = "Save file (Normal mode)" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Insert Mode mappings
+map("i", "jk", "<ESC>")
+map("i", "<leader>w", "<Esc>:w<CR>i", { desc = "Save file (Insert mode)" })
+
+-- Terminal Mode: Escape to Normal
+map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- LSP Mappings (Native)
+map("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+map("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
+map("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+map("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to References" })
+map("n", "<leader>k", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+
+-- Lspsaga-enhanced
+map("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
+map("n", "gh", "<cmd>Lspsaga finder<CR>", { desc = "Finder: Def/Impl/Refs" })
+map("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { desc = "Symbol Outline" })
